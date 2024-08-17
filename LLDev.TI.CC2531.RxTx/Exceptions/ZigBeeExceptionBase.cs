390b019466f0dfd5e0a32b05dc8c11f195ae4a83
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-17 10:34:58 +0300

    Adding ZigBeeExceptionBase class

---
+++ LLDev.TI.CC2531.RxTx/Exceptions/ZigBeeExceptionBase.cs
@@ -0,0 +1,17 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Exceptions;
+public class ZigBeeExceptionBase : ApplicationException
+{
+    protected ZigBeeExceptionBase()
+    {
+    }
+
+    protected ZigBeeExceptionBase(string message) :
+        base(message)
+    {
+    }
+
+    protected ZigBeeExceptionBase(string message, Exception inner) :
+        base(message, inner)
+    {
+    }
+}
