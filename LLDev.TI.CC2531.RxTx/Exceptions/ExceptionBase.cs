e47e55c4d9dd9294c58d0128fd21e2fd81ec4fb0
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-17 10:35:49 +0300

    Renaming ZigBeeExceptionBase to ExceptionBase

---
+++ LLDev.TI.CC2531.RxTx/Exceptions/ExceptionBase.cs
@@ -0,0 +1,17 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Exceptions;
+public class ExceptionBase : ApplicationException
+{
+    protected ExceptionBase()
+    {
+    }
+
+    protected ExceptionBase(string message) :
+        base(message)
+    {
+    }
+
+    protected ExceptionBase(string message, Exception inner) :
+        base(message, inner)
+    {
+    }
+}
