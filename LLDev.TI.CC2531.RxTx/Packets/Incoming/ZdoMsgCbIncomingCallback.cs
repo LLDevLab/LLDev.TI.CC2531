98bff9cadd50ea59deea8a9e5f253395a3d13ec0
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 17:20:01 +0300

    Adding ZdoMsgCbIncomingCallback class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/ZdoMsgCbIncomingCallback.cs
@@ -0,0 +1,22 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+public sealed class ZdoMsgCbIncomingCallback : IncomingPacket, IIncomingPacket
+{
+    public ushort SrcAddr { get; }
+    public bool IsBroadcast { get; }
+    public ZigBeeClusterId ClusterId { get; }
+    public byte SeqNum { get; }
+    public ushort MacDstAddr { get; }
+    public byte[] MsgData { get; }
+    public ZdoMsgCbIncomingCallback(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x09)
+    {
+        SrcAddr = GetUShort(Data[1], Data[0]);
+        IsBroadcast = Data[2] != 0;
+        ClusterId = (ZigBeeClusterId)GetUShort(Data[4], Data[3]);
+        SeqNum = Data[6];
+        MacDstAddr = GetUShort(Data[8], Data[7]);
+        MsgData = Data[9..];
+    }
+}
