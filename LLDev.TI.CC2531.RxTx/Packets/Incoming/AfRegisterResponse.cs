d35f9ae33fea05c352e053518af71dfd33d15b54
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 11:16:06 +0300

    Adding AfRegisterResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/AfRegisterResponse.cs
@@ -0,0 +1,19 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+
+public interface IAfRegisterResponse : IIncomingPacket
+{
+    ZToolPacketStatus Status { get; }
+}
+
+public sealed class AfRegisterResponse : IncomingPacket, IAfRegisterResponse
+{
+    public ZToolPacketStatus Status { get; }
+
+    public AfRegisterResponse(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x01)
+    {
+        Status = (ZToolPacketStatus)Data[0];
+    }
+}
