98285c952eefc81ebaaabef9234216271e3a6810
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 16:34:36 +0300

    Adding SysOsalNvLengthResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/SysOsalNvLengthResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+public interface ISysOsalNvLengthResponse : IIncomingPacket
+{
+    ushort Length { get; }
+}
+
+public sealed class SysOsalNvLengthResponse : IncomingPacket, ISysOsalNvLengthResponse
+{
+    /// <summary>
+    /// 0x0000 = item does not exist, 0x0001-0xNNNN = number of bytes in NV item
+    /// </summary>
+    public ushort Length { get; }
+
+    public SysOsalNvLengthResponse(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x02)
+    {
+        Length = GetUShort(Data[1], Data[0]);
+    }
+}
