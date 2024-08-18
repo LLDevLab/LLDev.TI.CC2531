c64b79868bfe497a364b832cb434f374b94da642
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 10:37:24 +0300

    Adding PacketException class

---
+++ LLDev.TI.CC2531.RxTx/Exceptions/PacketException.cs
@@ -0,0 +1,15 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Exceptions;
+public sealed class PacketException : ExceptionBase
+{
+    public PacketException()
+    {
+    }
+
+    public PacketException(string message) : base(message)
+    {
+    }
+
+    public PacketException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
