427fce924c8b47bf1056139da1b7cb890041ac72
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 17:45:55 +0300

    Adding UtilLedControlResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/UtilLedControlResponse.cs
@@ -0,0 +1,12 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+public sealed class UtilLedControlResponse : IncomingPacket, IIncomingPacket
+{
+    public ZToolPacketStatus Status { get; }
+    public UtilLedControlResponse(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x01)
+    {
+        Status = (ZToolPacketStatus)Data[0];
+    }
+}
