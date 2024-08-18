e4c27560497bac7bfcf4bd2476ca904c8b333218
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 16:36:13 +0300

    Adding SysOsalNvReadResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/SysOsalNvReadResponse.cs
@@ -0,0 +1,38 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+
+public interface ISysOsalNvReadResponse : IIncomingPacket
+{
+    ZToolPacketStatus Status { get; }
+    byte Len { get; }
+    byte[] Value { get; }
+}
+
+public sealed class SysOsalNvReadResponse : IncomingPacket, ISysOsalNvReadResponse
+{
+    public ZToolPacketStatus Status { get; }
+    public byte Len { get; }
+    public byte[] Value { get; }
+
+    public SysOsalNvReadResponse(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x02)
+    {
+        Status = (ZToolPacketStatus)Data[0];
+        Len = Data[1];
+        Value = GetValue();
+    }
+
+    private byte[] GetValue()
+    {
+        if (Len == 0)
+            return [];
+
+        var result = new byte[Len];
+
+        for (var i = 0; i < Len; i++)
+            result[i] = Data[i + 2];
+
+        return result;
+    }
+}
