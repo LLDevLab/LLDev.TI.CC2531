c4a9bd91bd883eacb581477a62b76a1a5852b9e3
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 11:07:33 +0300

    Adding AfDataResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/AfDataResponse.cs
@@ -0,0 +1,19 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+
+public interface IAfDataResponse : IIncomingPacket
+{
+    ZToolPacketStatus Status { get; }
+}
+
+public sealed class AfDataResponse : IncomingPacket, IAfDataResponse
+{
+    public ZToolPacketStatus Status { get; }
+
+    public AfDataResponse(IPacketHeader packetHeader, byte[] packet) :
+        base(packetHeader, packet, 0x01)
+    {
+        Status = (ZToolPacketStatus)Data[0];
+    }
+}
