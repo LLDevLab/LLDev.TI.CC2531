e8b73c173105869134e51f134f31ae02a47d2bd4
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 17:13:21 +0300

    Adding ZdoExtRouteDiscoveryResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/ZdoExtRouteDiscoveryResponse.cs
@@ -0,0 +1,13 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+public sealed class ZdoExtRouteDiscoveryResponse : IncomingPacket, IIncomingPacket
+{
+    public ZToolPacketStatus Status { get; }
+
+    public ZdoExtRouteDiscoveryResponse(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x01)
+    {
+        Status = (ZToolPacketStatus)Data[0];
+    }
+}
