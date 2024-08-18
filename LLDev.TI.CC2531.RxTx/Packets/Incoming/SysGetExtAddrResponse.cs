e8c2c22489d63be9c3a4b06a9a8bf1c7947931e0
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 16:31:40 +0300

    Adding SysGetExtAddrResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/SysGetExtAddrResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+
+public interface ISysGetExtAddrResponse : IIncomingPacket
+{
+    ulong ExtAddress { get; }
+}
+
+public sealed class SysGetExtAddrResponse : IncomingPacket, ISysGetExtAddrResponse
+{
+    /// <summary>
+    /// The device’s extended address.
+    /// </summary>
+    public ulong ExtAddress { get; }
+
+    public SysGetExtAddrResponse(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x08)
+    {
+        ExtAddress = GetBigEndianULong(Data);
+    }
+}
