379311a65f3fdf8ca840b9042935df1f86168aba
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 17:41:31 +0300

    Adding ZdoStateChangedIndCallback class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/ZdoStateChangedIndCallback.cs
@@ -0,0 +1,12 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+public sealed class ZdoStateChangedIndCallback : IncomingPacket, IIncomingPacket
+{
+    public ZToolZdoState State { get; }
+    public ZdoStateChangedIndCallback(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x01)
+    {
+        State = (ZToolZdoState)Data[0];
+    }
+}
