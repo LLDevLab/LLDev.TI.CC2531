fc40e74b8f29b42ef35c1721d64212528777851a
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 17:40:13 +0300

    Adding ZdoStartupFromAppResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/ZdoStartupFromAppResponse.cs
@@ -0,0 +1,12 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+public sealed class ZdoStartupFromAppResponse : IncomingPacket, IIncomingPacket
+{
+    public ZToolZdoStartupFromAppStatus Status { get; }
+    public ZdoStartupFromAppResponse(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x01)
+    {
+        Status = (ZToolZdoStartupFromAppStatus)Data[0];
+    }
+}
