1db21c46b5bae24752b0e4593a4dfa8044a90b1a
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 10:30:32 +0300

    Adding ByteArrayExtension and it unit tests

---
+++ LLDev.TI.CC2531.RxTx.Tests/Extensions/ByteArrayExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using LLDev.TI.CC2531.RxTx.Extensions;
+
+namespace LLDev.TI.CC2531.RxTx.Tests.Extensions;
+public class ByteArrayExtensionTests
+{
+    [Fact]
+    public void ArrayToString()
+    {
+        // Arrange.
+        var arr = new byte[] { 1, 2, 3 };
+
+        // Act.
+        var result = arr.ArrayToString();
+
+        // Assert.
+        Assert.Equal("01-02-03", result);
+    }
+}
---
+++ LLDev.TI.CC2531.RxTx/Extensions/ByteArrayExtension.cs
@@ -0,0 +1,5 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Extensions;
+public static class ByteArrayExtension
+{
+    public static string ArrayToString(this byte[] data) => BitConverter.ToString(data);
+}
