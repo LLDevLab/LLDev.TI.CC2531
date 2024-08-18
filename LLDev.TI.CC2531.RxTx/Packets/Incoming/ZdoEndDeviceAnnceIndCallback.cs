5a0d416da479733d9c70aed4614923241e7655c3
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 17:08:55 +0300

    Adding ZdoEndDeviceAnnceIndCallback class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/ZdoEndDeviceAnnceIndCallback.cs
@@ -0,0 +1,24 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+public sealed class ZdoEndDeviceAnnceIndCallback : IncomingPacket, IIncomingPacket
+{
+    public ushort SrcAddr { get; }
+    public ushort NwkAddr { get; }
+    public ulong IeeeAddr { get; }
+    public bool IsAlternativePanCoordinator => _capabilities.HasFlag(ZToolEndDevCaps.AltPanCoord);
+    public bool IsZigBeeRouter => _capabilities.HasFlag(ZToolEndDevCaps.DeviceType);
+    public bool IsMainPowered => _capabilities.HasFlag(ZToolEndDevCaps.PowerSource);
+    public bool IsReceiverOnWhenIdle => _capabilities.HasFlag(ZToolEndDevCaps.ReceiverOnWhenIdle);
+    public bool IsSecure => _capabilities.HasFlag(ZToolEndDevCaps.Security);
+
+    private readonly ZToolEndDevCaps _capabilities;
+    public ZdoEndDeviceAnnceIndCallback(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x0d)
+    {
+        SrcAddr = GetUShort(Data[1], Data[0]);
+        NwkAddr = GetUShort(Data[3], Data[2]);
+        IeeeAddr = GetLittleEndianULong(Data[4..12]);
+        _capabilities = (ZToolEndDevCaps)Data[12];
+    }
+}
