311e14e31783348bd60bd34016387e9463ce5a02
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 17:29:23 +0300

    Adding ZToolZdoNodeDescRpsServerMask enum

---
+++ LLDev.TI.CC2531.RxTx/Enums/ZToolZdoNodeDescRpsServerMask.cs
@@ -0,0 +1,12 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Enums;
+
+[Flags]
+public enum ZToolZdoNodeDescRpsServerMask : ushort
+{
+    PrimaryTrustCenter = 0x01,
+    BackupTrustCenter = 0x02,
+    PrimaryBindTableCache = 0x04,
+    BackupBindTableCache = 0x08,
+    PrimaryDiscoveryCache = 0x10,
+    BackupDiscoveryCache = 0x20
+}
