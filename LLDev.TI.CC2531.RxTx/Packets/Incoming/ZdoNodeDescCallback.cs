fe6213540929f460ef6cbf5d4aead634fe2c4a9c
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 17:22:04 +0300

    Adding ZdoNodeDescCallback class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/ZdoNodeDescCallback.cs
@@ -0,0 +1,12 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+public sealed class ZdoNodeDescCallback : IncomingPacket, IIncomingPacket
+{
+    public ZToolPacketStatus Status { get; }
+    public ZdoNodeDescCallback(IPacketHeader header, byte[] data) :
+        base(header, data, 0x01)
+    {
+        Status = (ZToolPacketStatus)Data[0];
+    }
+}
