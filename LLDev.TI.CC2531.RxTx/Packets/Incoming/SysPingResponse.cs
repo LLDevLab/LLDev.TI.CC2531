1a5e1058d91e27cc2e9617a58893bec94f39be7a
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-18 16:39:00 +0300

    Adding SysPingResponse class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Incoming/SysPingResponse.cs
@@ -0,0 +1,39 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Incoming;
+
+public interface ISysPingResponse : IIncomingPacket
+{
+    bool IsSysCapable { get; }
+    bool IsMacCapable { get; }
+    bool IsNwkCapable { get; }
+    bool IsAfCapable { get; }
+    bool IsZdoCapable { get; }
+    bool IsSapiCapable { get; }
+    bool IsUtilCapable { get; }
+    bool IsDebugCapable { get; }
+    bool IsAppCapable { get; }
+    bool IsZoadCapable { get; }
+}
+
+public sealed class SysPingResponse : IncomingPacket, ISysPingResponse
+{
+    public bool IsSysCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapSys);
+    public bool IsMacCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapMac);
+    public bool IsNwkCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapNwk);
+    public bool IsAfCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapAf);
+    public bool IsZdoCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapZdo);
+    public bool IsSapiCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapSapi);
+    public bool IsUtilCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapUtil);
+    public bool IsDebugCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapDebug);
+    public bool IsAppCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapApp);
+    public bool IsZoadCapable => _capabilities.HasFlag(ZToolStackCapabilities.MtCapZoad);
+
+    private readonly ZToolStackCapabilities _capabilities;
+
+    public SysPingResponse(IPacketHeader header, byte[] packet) :
+        base(header, packet, 0x02)
+    {
+        _capabilities = (ZToolStackCapabilities)GetUShort(Data[1], Data[0]);
+    }
+}
