9967b9c9c81a923aa5a1874fbe88c29c32f65ff1
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-20 20:42:10 +0300

    Adding OutgoingPacket class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/OutgoingPacket.cs
@@ -0,0 +1,17 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+
+public interface IOutgoingPacket : IPacket
+{
+}
+
+public abstract class OutgoingPacket(ZToolCmdType requestType, byte dataLen) :
+    Packet(new PacketHeader([StartByte, dataLen, GetCmd0(requestType), GetCmd1(requestType)])), IOutgoingPacket
+{
+    protected static byte GetLsb(ushort val) => (byte)val;
+    protected static byte GetMsb(ushort val) => (byte)(val >> 8);
+
+    private static byte GetCmd0(ZToolCmdType type) => GetMsb((ushort)type);
+    private static byte GetCmd1(ZToolCmdType type) => GetLsb((ushort)type);
+}
