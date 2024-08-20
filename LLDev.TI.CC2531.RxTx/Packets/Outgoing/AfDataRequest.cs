27214401251483ff5c9eec05629fa498559ab545
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-20 20:50:41 +0300

    Adding AfDataRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/AfDataRequest.cs
@@ -0,0 +1,43 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class AfDataRequest : OutgoingPacket
+{
+    public byte TransactionId { get; }
+
+    protected sealed override byte[] Data { get; }
+
+    public AfDataRequest(ushort nwkDstAddr,
+        byte dstEndpoint,
+        byte srcEndpoint,
+        ZigBeeClusterId clusterId,
+        byte transId,
+        byte options,
+        byte radius,
+        sbyte requestDataLen,
+        byte[] requestData) : base(ZToolCmdType.AfDataReq, (byte)(10 + requestDataLen))
+    {
+        var dataList = new List<byte>();
+        dataList.AddRange(GetBytesFromUshort(nwkDstAddr));
+        dataList.Add(dstEndpoint);
+        dataList.Add(srcEndpoint);
+        dataList.AddRange(GetBytesFromUshort((ushort)clusterId));
+        dataList.Add(transId);
+        dataList.Add(options);
+        dataList.Add(radius);
+        dataList.Add((byte)requestDataLen);
+        dataList.AddRange(requestData);
+
+        Data = [.. dataList];
+
+        TransactionId = transId;
+    }
+
+    private static byte[] GetBytesFromUshort(ushort value)
+    {
+        var bytes = new byte[2];
+        bytes[0] = (byte)(value >> 8);
+        bytes[1] = (byte)value;
+        return bytes;
+    }
+}
