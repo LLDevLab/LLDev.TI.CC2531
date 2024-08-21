03d5287786df640a4e0112f3e3d54ec95fa4ff23
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-21 21:03:15 +0300

    Adding SysGetExtAddrRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/SysGetExtAddrRequest.cs
@@ -0,0 +1,13 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class SysGetExtAddrRequest : OutgoingPacket, IOutgoingPacket
+{
+    protected override byte[] Data { get; }
+
+    public SysGetExtAddrRequest() :
+        base(ZToolCmdType.SysGetExtAddrReq, 0x00)
+    {
+        Data = [];
+    }
+}
