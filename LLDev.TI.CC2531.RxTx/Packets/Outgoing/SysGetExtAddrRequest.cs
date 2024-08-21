664b4014fe558772abd603537ecfbf1f88501af7
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-21 21:04:44 +0300

    Using primary constructor in SysGetExtAddrRequest class

--- LLDev.TI.CC2531.RxTx/Packets/Outgoing/SysGetExtAddrRequest.cs
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/SysGetExtAddrRequest.cs
@@ -1,13 +1,7 @@
 ﻿using LLDev.TI.CC2531.RxTx.Enums;
 
 namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
-public sealed class SysGetExtAddrRequest : OutgoingPacket, IOutgoingPacket
+public sealed class SysGetExtAddrRequest() : OutgoingPacket(ZToolCmdType.SysGetExtAddrReq, 0x00), IOutgoingPacket
 {
-    protected override byte[] Data { get; }
-
-    public SysGetExtAddrRequest() :
-        base(ZToolCmdType.SysGetExtAddrReq, 0x00)
-    {
-        Data = [];
-    }
+    protected override byte[] Data { get; } = [];
 }
