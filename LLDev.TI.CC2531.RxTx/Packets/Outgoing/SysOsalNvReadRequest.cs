a04b2e9e2cdeb8cdcbd2b8a5ceadb3997ae815f3
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-21 21:10:39 +0300

    Adding SysOsalNvReadRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/SysOsalNvReadRequest.cs
@@ -0,0 +1,25 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class SysOsalNvReadRequest : OutgoingPacket, IOutgoingPacket
+{
+    /// <summary>
+    /// The Id of the NV item.
+    /// </summary>
+    public ushort Id { get; }
+
+    /// <summary>
+    /// Number of bytes offset from the beginning or the NV value.
+    /// </summary>
+    public byte Offset { get; }
+
+    protected override byte[] Data { get; }
+
+    public SysOsalNvReadRequest(ushort id, byte offset) :
+        base(ZToolCmdType.SysOsalNvReadReq, 0x03)
+    {
+        Id = id;
+        Offset = offset;
+        Data = [.. BitConverter.GetBytes(Id), Offset];
+    }
+}
