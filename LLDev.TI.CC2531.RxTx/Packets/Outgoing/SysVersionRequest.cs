1c63adf2265d6876d6cf192e16c56853fe3b4128
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-22 21:11:25 +0300

    Adding SysVersionRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/SysVersionRequest.cs
@@ -0,0 +1,7 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class SysVersionRequest() : OutgoingPacket(ZToolCmdType.SysVersionReq, 0x00), IOutgoingPacket
+{
+    protected override byte[] Data => [];
+}
