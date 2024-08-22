cbac3898678202f33feeebd8db05a44f7cda571a
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-22 21:31:45 +0300

    Adding ZdoActiveEpRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/ZdoActiveEpRequest.cs
@@ -0,0 +1,12 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+using System.Buffers.Binary;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class ZdoActiveEpRequest(ushort destAddr, ushort nwkAddrOfInterest) : OutgoingPacket(ZToolCmdType.ZdoActiveEpReq, 4), IOutgoingPacket
+{
+    public ushort DestAddr { get; } = destAddr;
+    public ushort NwkAddrOfInterest { get; } = nwkAddrOfInterest;
+
+    protected override byte[] Data { get; } =
+        [.. BitConverter.GetBytes(BinaryPrimitives.ReverseEndianness(destAddr)), .. BitConverter.GetBytes(BinaryPrimitives.ReverseEndianness(nwkAddrOfInterest))];
+}
