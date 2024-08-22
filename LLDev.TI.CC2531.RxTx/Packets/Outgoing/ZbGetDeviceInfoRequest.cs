f8c70d072c2283e799b37f94fb0dff8de6c57bce
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-22 21:21:38 +0300

    Adding SysVersionRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/ZbGetDeviceInfoRequest.cs
@@ -0,0 +1,15 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class ZbGetDeviceInfoRequest : OutgoingPacket, IOutgoingPacket
+{
+    public DeviceInfoType InfoType { get; }
+    protected override byte[] Data { get; }
+
+    public ZbGetDeviceInfoRequest(DeviceInfoType infoType) :
+        base(ZToolCmdType.ZbGetDeviceInfoReq, 1)
+    {
+        InfoType = infoType;
+        Data = [(byte)InfoType];
+    }
+}
