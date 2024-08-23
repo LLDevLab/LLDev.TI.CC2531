5fd4f8d316988f9e6c8c3a02cc3e9e376d8d24bb
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-23 21:28:02 +0300

    Adding ZdoNwkDiscoveryRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/ZdoNwkDiscoveryRequest.cs
@@ -0,0 +1,23 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+using System.Buffers.Binary;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class ZdoNwkDiscoveryRequest : OutgoingPacket, IOutgoingPacket
+{
+    public ZToolDiscoveryScanChannelTypes ChannelTypes { get; }
+    public byte ScanDuration { get; }
+
+    protected override byte[] Data { get; }
+
+    public ZdoNwkDiscoveryRequest(ZToolDiscoveryScanChannelTypes channelTypes, byte scanDuration) :
+        base(ZToolCmdType.ZdoNwkDiscoveryReq, 0x05)
+    {
+        ChannelTypes = channelTypes;
+        ScanDuration = scanDuration;
+
+        var dataList = new List<byte>();
+        dataList.AddRange(BitConverter.GetBytes(BinaryPrimitives.ReverseEndianness((uint)channelTypes)));
+        dataList.Add(ScanDuration);
+        Data = [.. dataList];
+    }
+}
