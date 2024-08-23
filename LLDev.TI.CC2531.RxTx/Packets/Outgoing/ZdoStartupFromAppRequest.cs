60e24775ebd2984f8cab7722a66bce563750dc1d
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-23 21:32:54 +0300

    Adding ZdoStartupFromAppRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/ZdoStartupFromAppRequest.cs
@@ -0,0 +1,9 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class ZdoStartupFromAppRequest(ushort startDelay) : OutgoingPacket(ZToolCmdType.ZdoStartupFromAppReq, 2), IOutgoingPacket
+{
+    public ushort StartDelay { get; } = startDelay;
+
+    protected override byte[] Data { get; } = [.. BitConverter.GetBytes(startDelay)];
+}
