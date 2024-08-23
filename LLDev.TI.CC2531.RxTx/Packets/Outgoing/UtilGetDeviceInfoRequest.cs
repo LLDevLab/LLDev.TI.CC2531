732095ba900b7df84e710b49aff61230fd9a1127
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-23 21:34:16 +0300

    Adding UtilGetDeviceInfoRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/UtilGetDeviceInfoRequest.cs
@@ -0,0 +1,7 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class UtilGetDeviceInfoRequest() : OutgoingPacket(ZToolCmdType.UtilGetDeviceInfoReq, 0), IOutgoingPacket
+{
+    protected override byte[] Data => [];
+}
