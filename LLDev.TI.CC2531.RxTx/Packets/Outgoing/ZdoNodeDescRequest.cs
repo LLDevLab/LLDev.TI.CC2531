c7f8189d616fc16e4c9d5ba4bbc8729a0890cc81
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-23 21:23:14 +0300

    Adding ZdoNodeDescRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/ZdoNodeDescRequest.cs
@@ -0,0 +1,27 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class ZdoNodeDescRequest : OutgoingPacket, IOutgoingPacket
+{
+    protected override byte[] Data { get; }
+
+    /// <summary>
+    /// Specifies NWK address of the device generating the inquiry
+    /// </summary>
+    public ushort DestAddr { get; }
+
+    /// <summary>
+    /// Specifies NWK address of the destination device being queried.
+    /// </summary>
+    public ushort NwkAddrOfInterest { get; }
+
+    /// <param name="destAddr">Specifies NWK address of the device generating the inquiry</param>
+    /// <param name="nwkAddrOfInterest">Specifies NWK address of the destination device being queried.</param>
+    public ZdoNodeDescRequest(ushort destAddr, ushort nwkAddrOfInterest) :
+        base(ZToolCmdType.ZdoNodeDescReq, 4)
+    {
+        DestAddr = destAddr;
+        NwkAddrOfInterest = nwkAddrOfInterest;
+        Data = [.. BitConverter.GetBytes(DestAddr), .. BitConverter.GetBytes(NwkAddrOfInterest)];
+    }
+}
