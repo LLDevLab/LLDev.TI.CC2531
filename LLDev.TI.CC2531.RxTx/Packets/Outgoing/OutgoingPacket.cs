ffbdf47810a9519454c7d257040e67a7069a0c97
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-23 21:45:36 +0300

    Removing GetLsb and GetMsb methods from OutgoingPacket class

--- LLDev.TI.CC2531.RxTx/Packets/Outgoing/OutgoingPacket.cs
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/OutgoingPacket.cs
@@ -1,4 +1,5 @@
 ﻿using LLDev.TI.CC2531.RxTx.Enums;
+using System.Buffers.Binary;
 
 namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
 
@@ -7,11 +8,6 @@ public interface IOutgoingPacket : IPacket
 }
 
 public abstract class OutgoingPacket(ZToolCmdType requestType, byte dataLen) :
-    Packet(new PacketHeader([StartByte, dataLen, GetCmd0(requestType), GetCmd1(requestType)])), IOutgoingPacket
+    Packet(new PacketHeader([StartByte, dataLen, .. BitConverter.GetBytes(BinaryPrimitives.ReverseEndianness((ushort)requestType))])), IOutgoingPacket
 {
-    protected static byte GetLsb(ushort val) => (byte)val;
-    protected static byte GetMsb(ushort val) => (byte)(val >> 8);
-
-    private static byte GetCmd0(ZToolCmdType type) => GetMsb((ushort)type);
-    private static byte GetCmd1(ZToolCmdType type) => GetLsb((ushort)type);
 }
