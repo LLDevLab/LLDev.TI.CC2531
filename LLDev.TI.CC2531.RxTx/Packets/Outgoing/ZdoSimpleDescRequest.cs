97ac300f166550f34caa32f23d537b6c3a51cb08
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-23 21:30:43 +0300

    Adding ZdoSimpleDescRequest class

---
+++ LLDev.TI.CC2531.RxTx/Packets/Outgoing/ZdoSimpleDescRequest.cs
@@ -0,0 +1,23 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+
+namespace LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+public sealed class ZdoSimpleDescRequest(ushort destAddr, ushort nwkAddrOfInterest, byte endpoint) :
+    OutgoingPacket(ZToolCmdType.ZdoSimpleDescReq, 5), IOutgoingPacket
+{
+    /// <summary>
+    /// Specifies NWK address of the device generating the inquiry.
+    /// </summary>
+    public ushort DestAddr { get; } = destAddr;
+
+    /// <summary>
+    /// Specifies NWK address of the destination device being queried.
+    /// </summary>
+    public ushort NwkAddrOfInterest { get; } = nwkAddrOfInterest;
+
+    /// <summary>
+    /// Specifies the application endpoint the data is from.
+    /// </summary>
+    public byte Endpoint { get; } = endpoint;
+
+    protected override byte[] Data { get; } = [.. BitConverter.GetBytes(destAddr), .. BitConverter.GetBytes(nwkAddrOfInterest), endpoint];
+}
