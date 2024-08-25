3902eb776d3a6a9d8b1b153badf950c7a0348389
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-25 14:31:15 +0300

    Using Constants.PacketHeaderLength in PacketHeaderFactory class

--- LLDev.TI.CC2531.RxTx/Packets/PacketHeaderFactory.cs
+++ LLDev.TI.CC2531.RxTx/Packets/PacketHeaderFactory.cs
@@ -10,12 +10,11 @@ public interface IPacketHeaderFactory
 public sealed class PacketHeaderFactory : IPacketHeaderFactory
 {
     private const byte StartByte = 0xfe;
-    private const int HeaderDataLength = 4;
 
     public IPacketHeader CreatePacketHeader(byte[] data)
     {
-        return data.Length != HeaderDataLength
-            ? throw new PacketException($"Connot create header. Data length is not equal {HeaderDataLength}.")
+        return data.Length != Constants.PacketHeaderLength
+            ? throw new PacketException($"Connot create header. Data length is not equal {Constants.PacketHeaderLength}.")
             : data[0] != StartByte ? throw new PacketException($"Cannot create header. Invalid packet start byte '{data[0]}'.") : new PacketHeader(data);
     }
 }
