9f3326f397ae214bc26445d1164d9cf43eed87f8
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-26 21:33:21 +0300

    Adding unit tests for SerialPortMessageHandler.Send method

--- LLDev.TI.CC2531.RxTx.Tests/Handlers/SerialPortMessageHandlerTests.cs
+++ LLDev.TI.CC2531.RxTx.Tests/Handlers/SerialPortMessageHandlerTests.cs
@@ -1,5 +1,7 @@
-﻿using LLDev.TI.CC2531.RxTx.Handlers;
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+using LLDev.TI.CC2531.RxTx.Handlers;
 using LLDev.TI.CC2531.RxTx.Packets;
+using LLDev.TI.CC2531.RxTx.Packets.Outgoing;
 using Microsoft.Extensions.Logging;
 
 namespace LLDev.TI.CC2531.RxTx.Tests.Handlers;
@@ -22,6 +24,49 @@ public class SerialPortMessageHandlerTests
         _serialPortDataHandlerMock.VerifyAdd(m => m.DataReceived += It.IsAny<SerialPortDataReceivedEventHandler>());
         _serialPortDataHandlerMock.VerifyRemove(m => m.DataReceived -= It.IsAny<SerialPortDataReceivedEventHandler>());
         _serialPortDataHandlerMock.Verify(m => m.Dispose());
+    }
+
+    [Fact]
+    public void Send_MessageAlreadySending_ThrowsInvalidOperationException()
+    {
+        // Arrange.
+        const ZToolCmdType ResultType = ZToolCmdType.SysResetIndClbk;
+
+        var outgoingPacketMock = new Mock<IOutgoingPacket>();
+
+        var handler = new SerialPortMessageHandler(_serialPortDataHandlerMock.Object, null!, null!, null!);
+
+        handler.Send(outgoingPacketMock.Object, null!, ResultType);
+
+        // Act. / Assert.
+        var result = Assert.Throws<InvalidOperationException>(() => handler.Send(outgoingPacketMock.Object, null!, ResultType));
+
+        _serialPortDataHandlerMock.VerifyAll();
+
+        Assert.Equal($"Request for result type {ResultType} already sending.", result.Message);
+    }
+
+    [Fact]
+    public void Send()
+    {
+        // Arrange.
+        const ZToolCmdType ResultType = ZToolCmdType.SysResetIndClbk;
+
+        var bytesToSend = new byte[] { 0x01, 0x02, 0x03 };
+
+        var outgoingPacketMock = new Mock<IOutgoingPacket>();
+        outgoingPacketMock.Setup(m => m.ToByteArray()).Returns(bytesToSend);
+
+        _serialPortDataHandlerMock.Setup(m => m.Write(bytesToSend));
+
+        var handler = new SerialPortMessageHandler(_serialPortDataHandlerMock.Object, null!, null!, null!);
+
+        // Act.
+        handler.Send(outgoingPacketMock.Object, null!, ResultType);
+
+        // Assert.
+        outgoingPacketMock.VerifyAll();
+        _serialPortDataHandlerMock.VerifyAll();
     }
 
     [Fact]
