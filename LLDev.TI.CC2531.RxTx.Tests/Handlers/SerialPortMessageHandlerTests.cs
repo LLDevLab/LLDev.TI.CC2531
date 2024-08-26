430702339e1bb260b6102af14da6973848249f72
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-26 21:30:25 +0300

    Adding ConstructAndDispose unit test to SerialPortMessageHandler tests

--- LLDev.TI.CC2531.RxTx.Tests/Handlers/SerialPortMessageHandlerTests.cs
+++ LLDev.TI.CC2531.RxTx.Tests/Handlers/SerialPortMessageHandlerTests.cs
@@ -1,6 +1,29 @@
-﻿namespace LLDev.TI.CC2531.RxTx.Tests.Handlers;
+﻿using LLDev.TI.CC2531.RxTx.Handlers;
+using LLDev.TI.CC2531.RxTx.Packets;
+using Microsoft.Extensions.Logging;
+
+namespace LLDev.TI.CC2531.RxTx.Tests.Handlers;
 public class SerialPortMessageHandlerTests
 {
+    private readonly Mock<ISerialPortDataHandler> _serialPortDataHandlerMock = new();
+    private readonly Mock<IPacketFactory> _packetFactoryMock = new();
+    private readonly Mock<ILogger<SerialPortMessageHandler>> _loggerMock = new();
+
+    [Fact]
+    public void ConstructAndDispose()
+    {
+        // Act.
+        using (var handler = new SerialPortMessageHandler(_serialPortDataHandlerMock.Object, null!, null!, null!))
+        {
+        }
+
+        // Assert.
+        _serialPortDataHandlerMock.Verify(m => m.Open());
+        _serialPortDataHandlerMock.VerifyAdd(m => m.DataReceived += It.IsAny<SerialPortDataReceivedEventHandler>());
+        _serialPortDataHandlerMock.VerifyRemove(m => m.DataReceived -= It.IsAny<SerialPortDataReceivedEventHandler>());
+        _serialPortDataHandlerMock.Verify(m => m.Dispose());
+    }
+
     [Fact]
     public void Test123() => Assert.Fail("Implement me");
 }
