9bf0454cf218b6cef785f03b2df21ec334820ad6
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-26 21:08:07 +0300

    Adding PacketHeaderException class

---
+++ LLDev.TI.CC2531.RxTx/Exceptions/PacketHeaderException.cs
@@ -0,0 +1,15 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Exceptions;
+public sealed class PacketHeaderException : ExceptionBase
+{
+    public PacketHeaderException()
+    {
+    }
+
+    public PacketHeaderException(string message) : base(message)
+    {
+    }
+
+    public PacketHeaderException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
