4f8ce2034fc261e9b8579d25602af107be7d721c
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-29 19:34:43 +0300

    Adding MessageCallbackMethodsCacheService and it unit tests

---
+++ LLDev.TI.CC2531.RxTx.Tests/Services/MessageCallbackMethodsCacheServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+using LLDev.TI.CC2531.RxTx.Exceptions;
+using LLDev.TI.CC2531.RxTx.Services;
+
+namespace LLDev.TI.CC2531.RxTx.Tests.Services;
+public class MessageCallbackMethodsCacheServiceTests
+{
+    [Fact]
+    public void GetAndRemove_KeyNotFound_ThrowsMessageException()
+    {
+        // Arrange.
+        const ZToolCmdType CmdType = ZToolCmdType.ZbGetDeviceInfoReq;
+
+        var service = new MessageCallbackMethodsCacheService();
+
+        // Act. / Assert.
+        Assert.Throws<MessageException>(() => service.GetAndRemove(CmdType));
+    }
+
+    [Fact]
+    public void EntireCacheTest()
+    {
+        // Arrange.
+        const ZToolCmdType CmdType = ZToolCmdType.ZbGetDeviceInfoReq;
+
+        var actionExecutionCount = 0;
+
+        var service = new MessageCallbackMethodsCacheService();
+
+        // Act 1.
+        var result1 = service.ContainsKey(CmdType);
+
+        // Assert 1.
+        Assert.False(result1);
+
+        // Act 2.
+        service.Add(CmdType, packetType => actionExecutionCount++);
+        var result2 = service.ContainsKey(CmdType);
+
+        // Assert 2.
+        Assert.True(result2);
+
+        // Act 3.
+        var action = service.GetAndRemove(CmdType);
+        var result3 = service.ContainsKey(CmdType);
+
+        // Assert.
+        Assert.False(result3);
+
+        // Act 4.
+        action(null);
+
+        Assert.Equal(1, actionExecutionCount);
+    }
+}
---
+++ LLDev.TI.CC2531.RxTx/Services/MessageCallbackMethodsCacheService.cs
@@ -0,0 +1,27 @@
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+using LLDev.TI.CC2531.RxTx.Exceptions;
+using LLDev.TI.CC2531.RxTx.Packets.Incoming;
+using System.Collections.Concurrent;
+
+namespace LLDev.TI.CC2531.RxTx.Services;
+
+internal interface IMessageCallbackMethodsCacheService
+{
+    void Add(ZToolCmdType key, Action<IIncomingPacket?> value);
+    bool ContainsKey(ZToolCmdType key);
+    Action<IIncomingPacket?> GetAndRemove(ZToolCmdType key);
+}
+
+internal sealed class MessageCallbackMethodsCacheService : IMessageCallbackMethodsCacheService
+{
+    private readonly ConcurrentDictionary<ZToolCmdType, Action<IIncomingPacket?>> _callbackMethods = new();
+
+    public void Add(ZToolCmdType key, Action<IIncomingPacket?> value) => _callbackMethods.TryAdd(key, value);
+    public bool ContainsKey(ZToolCmdType key) => _callbackMethods.ContainsKey(key);
+    public Action<IIncomingPacket?> GetAndRemove(ZToolCmdType key)
+    {
+        return !_callbackMethods.TryRemove(key, out var value)
+            ? throw new MessageException($"Cannot remove message {key} from cache")
+            : value;
+    }
+}
