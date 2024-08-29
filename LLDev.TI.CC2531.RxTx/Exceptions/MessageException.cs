7e612875a7067fa6106d7f0c48958f76ac3e9ce5
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-29 19:13:52 +0300

    Adding MessageException class

---
+++ LLDev.TI.CC2531.RxTx/Exceptions/MessageException.cs
@@ -0,0 +1,15 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Exceptions;
+public sealed class MessageException : ExceptionBase
+{
+    public MessageException()
+    {
+    }
+
+    public MessageException(string message) : base(message)
+    {
+    }
+
+    public MessageException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
