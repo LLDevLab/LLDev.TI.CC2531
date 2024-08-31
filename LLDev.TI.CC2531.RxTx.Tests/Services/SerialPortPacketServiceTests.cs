689ec205965ebc57ca9231eb299c0235289b30c4
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-31 12:22:03 +0300

    Adding SerialPortPacketService constructor and Send method unit tests

--- LLDev.TI.CC2531.RxTx.Tests/Services/SerialPortPacketServiceTests.cs
+++ LLDev.TI.CC2531.RxTx.Tests/Services/SerialPortPacketServiceTests.cs
@@ -1,6 +1,51 @@
-﻿namespace LLDev.TI.CC2531.RxTx.Tests.Services;
+﻿using LLDev.TI.CC2531.RxTx.Configs;
+using LLDev.TI.CC2531.RxTx.Handlers;
+using LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+using LLDev.TI.CC2531.RxTx.Services;
+using Microsoft.Extensions.Options;
+
+namespace LLDev.TI.CC2531.RxTx.Tests.Services;
 public class SerialPortPacketServiceTests
 {
+    private const int Timeout = 200;
+
+    private readonly Mock<ISerialPortMessageHandler> _serialPortMessageHandlerMock = new();
+
+    private readonly IOptions<SerialPortMessageServiceConfig> _options = Options.Create(new SerialPortMessageServiceConfig
+    {
+        MessageWaitTimeoutMs = Timeout,
+    });
+
     [Fact]
-    public void TestFail() => Assert.Fail("Implement me");
+    public void CondtructAndDispose()
+    {
+        // Arrange. / Act.
+        using (var service = new SerialPortPacketService(_serialPortMessageHandlerMock.Object,
+            null!,
+            _options))
+        {
+        }
+
+        // Assert.
+        _serialPortMessageHandlerMock.VerifyAdd(m => m.MessageReceivedAsync += It.IsAny<MessageReceivedHandler>());
+        _serialPortMessageHandlerMock.VerifyRemove(m => m.MessageReceivedAsync -= It.IsAny<MessageReceivedHandler>());
+    }
+
+    [Fact]
+    public void Send()
+    {
+        // Arrange.
+        var outgoingPacketMock = new Mock<IOutgoingPacket>();
+
+        using var service = new SerialPortPacketService(_serialPortMessageHandlerMock.Object, null!, _options);
+
+        // Act.
+        service.Send(outgoingPacketMock.Object);
+
+        // Assert.
+        _serialPortMessageHandlerMock.Verify(m => m.Send(outgoingPacketMock.Object), Times.Once);
+    }
+
+    [Fact]
+    public void SendAndWaitForResponse() => Assert.Fail("Implement me");
 }
