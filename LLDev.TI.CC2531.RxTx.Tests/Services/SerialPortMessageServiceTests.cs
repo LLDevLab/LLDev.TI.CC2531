c884cb6542705ccfb2cfae430ac5bcd5ca138d52
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-08-31 10:49:35 +0300

    Adding SerialPortMessageService class

---
+++ LLDev.TI.CC2531.RxTx.Tests/Services/SerialPortMessageServiceTests.cs
@@ -0,0 +1,6 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Tests.Services;
+public class SerialPortMessageServiceTests
+{
+    [Fact]
+    public void TestFail() => Assert.Fail("Implement me");
+}
---
+++ LLDev.TI.CC2531.RxTx/Services/SerialPortMessageService.cs
@@ -0,0 +1,90 @@
+﻿using LLDev.TI.CC2531.RxTx.Configs;
+using LLDev.TI.CC2531.RxTx.Enums;
+using LLDev.TI.CC2531.RxTx.Exceptions;
+using LLDev.TI.CC2531.RxTx.Handlers;
+using LLDev.TI.CC2531.RxTx.Packets.Incoming;
+using LLDev.TI.CC2531.RxTx.Packets.Outgoing;
+using Microsoft.Extensions.Options;
+
+namespace LLDev.TI.CC2531.RxTx.Services;
+
+internal interface ISerialPortMessageService
+{
+    event MessageReceivedHandler? MessageReceived;
+    void Send(IOutgoingPacket packet);
+    T SendAndWaitForResponse<T>(IOutgoingPacket packet, ZToolCmdType responseType) where T : IIncomingPacket;
+}
+
+internal sealed class SerialPortMessageService : ISerialPortMessageService
+{
+    public event MessageReceivedHandler? MessageReceived;
+    private event MessageReceivedHandler? AwaitedMessageReceived;
+
+    private readonly ISerialPortMessageHandler _messageHandler;
+    private readonly IAwaitedPacketCacheService _awaitedMessageCacheService;
+    private readonly SerialPortMessageServiceConfig _config;
+
+    public SerialPortMessageService(ISerialPortMessageHandler messageHandler,
+        IAwaitedPacketCacheService awaitedMessageCacheService,
+        IOptions<SerialPortMessageServiceConfig> options)
+    {
+        _messageHandler = messageHandler;
+        _awaitedMessageCacheService = awaitedMessageCacheService;
+        _config = options.Value;
+
+        _messageHandler.MessageReceivedAsync += OnMessageReceivedInternal;
+    }
+
+    public void Send(IOutgoingPacket packet) => _messageHandler.Send(packet);
+
+    public T SendAndWaitForResponse<T>(IOutgoingPacket packet, ZToolCmdType responseType) where T : IIncomingPacket
+    {
+        if (_awaitedMessageCacheService.Contains(responseType))
+            throw new PacketException($"Already awaiting packet {responseType}");
+
+        var timeout = _config.MessageWaitTimeoutMs;
+
+        IIncomingPacket? response = null;
+
+        using var manualResetEvent = new ManualResetEventSlim(false);
+
+        AwaitedMessageReceived += OnAwaitedMessageReceived;
+
+        _awaitedMessageCacheService.Add(responseType);
+
+        _messageHandler.Send(packet);
+
+        try
+        {
+            return !manualResetEvent.Wait(timeout)
+                ? throw new TimeoutException($"Cannot receive response within specified duretion {timeout} ms")
+                : response is null
+                ? throw new PacketException("Awaited packet cannot be null")
+                : response is not T result
+                ? throw new PacketException($"Cannot cast packet of type {response.GetType()} to {nameof(T)}")
+                : result;
+        }
+        finally
+        {
+            AwaitedMessageReceived -= OnAwaitedMessageReceived;
+        }
+
+        void OnAwaitedMessageReceived(IIncomingPacket packet)
+        {
+            if (packet.CmdType != responseType)
+                return;
+
+            _awaitedMessageCacheService.Remove(responseType);
+            response = packet;
+            manualResetEvent.Set();
+        }
+    }
+
+    private void OnMessageReceivedInternal(IIncomingPacket packet)
+    {
+        if (_awaitedMessageCacheService.Contains(packet.CmdType))
+            AwaitedMessageReceived?.Invoke(packet);
+        else
+            MessageReceived?.Invoke(packet);
+    }
+}
