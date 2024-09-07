d4c6a2418572a7e92975f1f7c48b8c45c61fa166
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-09-07 11:42:34 +0300

    Adding DiviceAnnounceInfo class record

---
+++ LLDev.TI.CC2531.RxTx/Models/DiviceAnnounceInfo.cs
@@ -0,0 +1,2 @@
+﻿namespace LLDev.TI.CC2531.RxTx.Models;
+public sealed record DiviceAnnounceInfo(ulong IeeeAddr, ushort NwkAddr, ushort SrcAddr, bool IsMainPowered, bool IsReceiverOnWhenIdle, bool IsSecure);
