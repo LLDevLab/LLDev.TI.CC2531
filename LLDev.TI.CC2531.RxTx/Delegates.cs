de9848e024000f59f77473e4a589491e1d1f9455
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-09-07 11:44:17 +0300

    Adding DeviceAnnouncedHandler delegate

--- LLDev.TI.CC2531.RxTx/Delegates.cs
+++ LLDev.TI.CC2531.RxTx/Delegates.cs
@@ -1,6 +1,8 @@
-﻿using LLDev.TI.CC2531.RxTx.Packets.Incoming;
+﻿using LLDev.TI.CC2531.RxTx.Models;
+using LLDev.TI.CC2531.RxTx.Packets.Incoming;
 
 namespace LLDev.TI.CC2531.RxTx;
 
 internal delegate void SerialPortDataReceivedEventHandler();
 internal delegate void MessageReceivedHandler(IIncomingPacket packet);
+public delegate Task DeviceAnnouncedHandler(DiviceAnnounceInfo diviceAnnounceInfo);
