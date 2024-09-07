64c0f8ace72f6a2e1a0c8259b6263349dc1f5d90
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-09-07 11:46:52 +0300

    Adding EndDeviceDataReceivedHandler delegate

--- LLDev.TI.CC2531.RxTx/Delegates.cs
+++ LLDev.TI.CC2531.RxTx/Delegates.cs
@@ -1,8 +1,11 @@
-﻿using LLDev.TI.CC2531.RxTx.Models;
+﻿using LLDev.TI.CC2531.RxTx.Enums;
+using LLDev.TI.CC2531.RxTx.Models;
 using LLDev.TI.CC2531.RxTx.Packets.Incoming;
 
 namespace LLDev.TI.CC2531.RxTx;
 
 internal delegate void SerialPortDataReceivedEventHandler();
 internal delegate void MessageReceivedHandler(IIncomingPacket packet);
+internal delegate Task EndDeviceDataReceivedHandler(ushort nwkAddr, ZigBeeClusterId clusterId, byte[] message);
+
 public delegate Task DeviceAnnouncedHandler(DiviceAnnounceInfo diviceAnnounceInfo);
