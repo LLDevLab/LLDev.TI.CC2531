788989dc84bcacb0a3c3324759aa78284b8fd346
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-09-08 11:06:37 +0300

    Adding NetworkCoordinatorService and it unit tests

---
+++ LLDev.TI.CC2531.RxTx.Tests/Services/NetworkCoordinatorServiceTests.cs
@@ -0,0 +1,251 @@
+﻿using LLDev.TI.CC2531.RxTx.Devices;
+using LLDev.TI.CC2531.RxTx.Enums;
+using LLDev.TI.CC2531.RxTx.Exceptions;
+using LLDev.TI.CC2531.RxTx.Models;
+using LLDev.TI.CC2531.RxTx.Services;
+using Microsoft.Extensions.Logging;
+
+namespace LLDev.TI.CC2531.RxTx.Tests.Services;
+
+public class NetworkCoordinatorServiceTests
+{
+    private readonly Mock<INetworkCoordinator> _networkCoordinatorMock = new();
+    private readonly Mock<ITransactionService> _transactionServiceMock = new();
+    private readonly Mock<ILogger<NetworkCoordinatorService>> _loggerMock = new();
+
+    [Fact]
+    public void StartupCoordinator_StartupFailed_ThrowsNetworkException()
+    {
+        // Arrange.
+        const ushort StartupDelay = 100;
+
+        _loggerMock.Setup(m => m.IsEnabled(LogLevel.Information)).Returns(true);
+
+        _networkCoordinatorMock.Setup(m => m.StartupNetwork(StartupDelay)).Returns(false);
+
+        var service = new NetworkCoordinatorService(_networkCoordinatorMock.Object,
+            null!,
+            _loggerMock.Object);
+
+        // Act. / Assert.
+        var exception = Assert.Throws<NetworkException>(service.StartupCoordinator);
+
+        _loggerMock.VerifyAll();
+        _networkCoordinatorMock.VerifyAll();
+
+        _networkCoordinatorMock.Verify(m => m.PingCoordinatorOrThrow(), Times.Once);
+        _networkCoordinatorMock.Verify(m => m.ResetCoordinator(), Times.Once);
+        _networkCoordinatorMock.Verify(m => m.GetCoordinatorInfo(), Times.Never);
+
+        _loggerMock.Verify(x => x.Log(LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.IsAny<It.IsAnyType>(),
+            It.IsAny<Exception>(),
+            It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
+    }
+
+    [Fact]
+    public void StartupCoordinator()
+    {
+        // Arrange.
+        const ushort StartupDelay = 100;
+
+        var deviceInfo = new DeviceInfo(123, 321);
+
+        _loggerMock.Setup(m => m.IsEnabled(LogLevel.Information)).Returns(true);
+
+        _networkCoordinatorMock.Setup(m => m.StartupNetwork(StartupDelay)).Returns(true);
+        _networkCoordinatorMock.Setup(m => m.GetCoordinatorInfo()).Returns(deviceInfo);
+
+        var service = new NetworkCoordinatorService(_networkCoordinatorMock.Object,
+            null!,
+            _loggerMock.Object);
+
+        // Act. 
+        var result = service.StartupCoordinator();
+
+        // Assert.
+        _loggerMock.VerifyAll();
+        _networkCoordinatorMock.VerifyAll();
+
+        _networkCoordinatorMock.Verify(m => m.PingCoordinatorOrThrow(), Times.Once);
+        _networkCoordinatorMock.Verify(m => m.ResetCoordinator(), Times.Once);
+
+        _loggerMock.Verify(x => x.Log(LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.IsAny<It.IsAnyType>(),
+            It.IsAny<Exception>(),
+            It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Exactly(2));
+
+        Assert.Equal(deviceInfo, result);
+    }
+
+    [Fact]
+    public void SetStatusLedMode_Failed_LogWarning()
+    {
+        // Arrange.
+        const byte StatusLedId = 1;
+        const bool LedEnable = true;
+
+        _networkCoordinatorMock.Setup(m => m.SetCoordinatorLedMode(StatusLedId, LedEnable)).Returns(false);
+        _loggerMock.Setup(m => m.IsEnabled(LogLevel.Warning)).Returns(true);
+
+        var service = new NetworkCoordinatorService(_networkCoordinatorMock.Object,
+            null!,
+            _loggerMock.Object);
+
+        // Act.
+        service.SetStatusLedMode(LedEnable);
+
+        // Assert.
+        _networkCoordinatorMock.VerifyAll();
+        _loggerMock.VerifyAll();
+
+        _loggerMock.Verify(x => x.Log(LogLevel.Warning,
+            It.IsAny<EventId>(),
+            It.IsAny<It.IsAnyType>(),
+            It.IsAny<Exception>(),
+            It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
+    }
+
+    [Fact]
+    public void SetStatusLedMode()
+    {
+        // Arrange.
+        const byte StatusLedId = 1;
+        const bool LedEnable = true;
+
+        _networkCoordinatorMock.Setup(m => m.SetCoordinatorLedMode(StatusLedId, LedEnable)).Returns(true);
+        _loggerMock.Setup(m => m.IsEnabled(LogLevel.Warning)).Returns(true);
+
+        var service = new NetworkCoordinatorService(_networkCoordinatorMock.Object,
+            null!,
+            _loggerMock.Object);
+
+        // Act.
+        service.SetStatusLedMode(LedEnable);
+
+        // Assert.
+        _networkCoordinatorMock.VerifyAll();
+
+        _loggerMock.Verify(x => x.Log(LogLevel.Warning,
+            It.IsAny<EventId>(),
+            It.IsAny<It.IsAnyType>(),
+            It.IsAny<Exception>(),
+            It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Never);
+    }
+
+    [Fact]
+    public void RegisterEndpoints_ThrowsNetworkException()
+    {
+        // Arrange.
+        const byte EndpointId1 = 123;
+        const byte EndpointId2 = 213;
+        const byte EndpointId3 = 221;
+
+        _loggerMock.Setup(m => m.IsEnabled(LogLevel.Information)).Returns(true);
+
+        var endpoint1 = new NetworkEndpoint(EndpointId1, 0, 0, 0, AfRegisterLatency.FastBeacons, 0, [], 0, []);
+        var endpoint2 = new NetworkEndpoint(EndpointId2, 0, 0, 0, AfRegisterLatency.FastBeacons, 0, [], 0, []);
+        var endpoint3 = new NetworkEndpoint(EndpointId3, 0, 0, 0, AfRegisterLatency.FastBeacons, 0, [], 0, []);
+
+        var mockSequence = new MockSequence();
+
+        _networkCoordinatorMock.InSequence(mockSequence).Setup(m => m.GetActiveEndpointIds()).Returns([EndpointId1]);
+        _networkCoordinatorMock.InSequence(mockSequence).Setup(m => m.GetSupportedEndpoints()).Returns([endpoint1, endpoint2, endpoint3]);
+        _networkCoordinatorMock.InSequence(mockSequence).Setup(m => m.GetActiveEndpointIds()).Returns([EndpointId1, EndpointId3]);
+
+        var service = new NetworkCoordinatorService(_networkCoordinatorMock.Object,
+            null!,
+            _loggerMock.Object);
+
+        // Act. / Assert.
+        var exception = Assert.Throws<NetworkException>(service.RegisterNetworkEndpoints);
+
+        _networkCoordinatorMock.VerifyAll();
+        _loggerMock.VerifyAll();
+
+        _networkCoordinatorMock.Verify(m => m.ValidateRegisteredEndpoints(It.IsAny<IEnumerable<byte>>()), Times.Never);
+
+        _loggerMock.Verify(x => x.Log(LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.IsAny<It.IsAnyType>(),
+            It.IsAny<Exception>(),
+            It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
+
+        Assert.Equal("Failed to register all endpoints.", exception.Message);
+    }
+
+    [Fact]
+    public void RegisterEndpoints_Success()
+    {
+        // Arrange.
+        const byte EndpointId1 = 123;
+        const byte EndpointId2 = 213;
+        const byte EndpointId3 = 221;
+
+        _loggerMock.Setup(m => m.IsEnabled(LogLevel.Information)).Returns(true);
+
+        var endpoint1 = new NetworkEndpoint(EndpointId1, 0, 0, 0, AfRegisterLatency.FastBeacons, 0, [], 0, []);
+        var endpoint2 = new NetworkEndpoint(EndpointId2, 0, 0, 0, AfRegisterLatency.FastBeacons, 0, [], 0, []);
+        var endpoint3 = new NetworkEndpoint(EndpointId3, 0, 0, 0, AfRegisterLatency.FastBeacons, 0, [], 0, []);
+
+        var mockSequence = new MockSequence();
+
+        _networkCoordinatorMock.InSequence(mockSequence).Setup(m => m.GetActiveEndpointIds()).Returns([EndpointId1]);
+        _networkCoordinatorMock.InSequence(mockSequence).Setup(m => m.GetSupportedEndpoints()).Returns([endpoint1, endpoint2, endpoint3]);
+        _networkCoordinatorMock.InSequence(mockSequence).Setup(m => m.GetActiveEndpointIds()).Returns(
+        [
+            EndpointId1,
+            EndpointId2,
+            EndpointId3
+        ]);
+
+        var service = new NetworkCoordinatorService(_networkCoordinatorMock.Object,
+            null!,
+            _loggerMock.Object);
+
+        // Act.
+        service.RegisterNetworkEndpoints();
+
+        // Assert.
+        _networkCoordinatorMock.VerifyAll();
+        _loggerMock.VerifyAll();
+
+        _networkCoordinatorMock.Verify(m => m.ValidateRegisteredEndpoints(It.Is<IEnumerable<byte>>(ba => ba.Count() == 3
+            && ba.All(i => i == EndpointId1 || i == EndpointId2 || i == EndpointId3))), Times.Once);
+
+        _loggerMock.Verify(x => x.Log(LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.IsAny<It.IsAnyType>(),
+            It.IsAny<Exception>(),
+            It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Exactly(2));
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void PermitNetworkJoin(bool expectedResult)
+    {
+        // Arrange.
+        const byte TransactionId = 123;
+        const bool IsPermitted = true;
+
+        _transactionServiceMock.Setup(m => m.GetNextTransactionId()).Returns(TransactionId);
+
+        _networkCoordinatorMock.Setup(m => m.PermitNetworkJoin(TransactionId, IsPermitted)).Returns(expectedResult);
+
+        var service = new NetworkCoordinatorService(_networkCoordinatorMock.Object,
+            _transactionServiceMock.Object,
+            null!);
+
+        // Act.
+        var result = service.PermitNetworkJoin(IsPermitted);
+
+        // Assert.
+        _transactionServiceMock.VerifyAll();
+        _networkCoordinatorMock.VerifyAll();
+
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ LLDev.TI.CC2531.RxTx/Services/NetworkCoordinatorService.cs
@@ -0,0 +1,80 @@
+﻿using LLDev.TI.CC2531.RxTx.Devices;
+using LLDev.TI.CC2531.RxTx.Exceptions;
+using LLDev.TI.CC2531.RxTx.Models;
+using Microsoft.Extensions.Logging;
+
+namespace LLDev.TI.CC2531.RxTx.Services;
+
+internal interface INetworkCoordinatorService
+{
+    DeviceInfo StartupCoordinator();
+    void SetStatusLedMode(bool value);
+    void RegisterNetworkEndpoints();
+    bool PermitNetworkJoin(bool isPermited);
+}
+
+internal sealed class NetworkCoordinatorService(INetworkCoordinator networkCoordinator,
+    ITransactionService transactionService,
+    ILogger<NetworkCoordinatorService> logger) : INetworkCoordinatorService
+{
+    private readonly INetworkCoordinator _networkCoordinator = networkCoordinator;
+    private readonly ITransactionService _transactionService = transactionService;
+    private readonly ILogger<NetworkCoordinatorService> _logger = logger;
+
+    public DeviceInfo StartupCoordinator()
+    {
+        const ushort StartupDelay = 100;
+
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("Starting up network coordinator.");
+
+        _networkCoordinator.PingCoordinatorOrThrow();
+        _networkCoordinator.ResetCoordinator();
+
+        if (!_networkCoordinator.StartupNetwork(StartupDelay))
+            throw new NetworkException("Cannot start up the network.");
+
+        var deviceInfo = _networkCoordinator.GetCoordinatorInfo();
+
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("Network coordinator succesfully started up.");
+
+        return deviceInfo;
+    }
+
+    public void SetStatusLedMode(bool value)
+    {
+        const int StatusLedId = 1;
+
+        if (!_networkCoordinator.SetCoordinatorLedMode(StatusLedId, value) && _logger.IsEnabled(LogLevel.Warning))
+            _logger.LogWarning("Failed to set status LED");
+    }
+
+    public void RegisterNetworkEndpoints()
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("Endpoints registration started.");
+
+        var activeEndpointIds = _networkCoordinator.GetActiveEndpointIds();
+
+        var endpointsToActivate = _networkCoordinator.GetSupportedEndpoints();
+
+        _networkCoordinator.RegisterEndpoints(endpointsToActivate.Where(x => !activeEndpointIds.Contains(x.EndpointId)));
+
+        activeEndpointIds = _networkCoordinator.GetActiveEndpointIds();
+
+        if (!endpointsToActivate.All(x => activeEndpointIds.Any(y => y == x.EndpointId)))
+            throw new NetworkException("Failed to register all endpoints.");
+
+        _networkCoordinator.ValidateRegisteredEndpoints(endpointsToActivate.Select(ep => ep.EndpointId));
+
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("Endpoints registration finished successfully.");
+    }
+
+    public bool PermitNetworkJoin(bool isPermited)
+    {
+        var transactionId = _transactionService.GetNextTransactionId();
+        return _networkCoordinator.PermitNetworkJoin(transactionId, isPermited);
+    }
+}
