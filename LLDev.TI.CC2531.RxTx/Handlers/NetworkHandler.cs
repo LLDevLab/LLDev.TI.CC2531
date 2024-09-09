0d56e5efcb180df88a9951a371b594ba67fdb503
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-09-09 21:00:40 +0300

    Changing DeviceInfo accessability from internal to public

---
+++ LLDev.TI.CC2531.RxTx/Handlers/NetworkHandler.cs
@@ -0,0 +1,48 @@
+﻿using LLDev.TI.CC2531.RxTx.Devices;
+using LLDev.TI.CC2531.RxTx.Models;
+using LLDev.TI.CC2531.RxTx.Services;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLDev.TI.CC2531.RxTx.Handlers;
+
+public interface INetworkHandler
+{
+    event DeviceAnnouncedHandler? DeviceAnnouncedAsync;
+    event EndDeviceMessageReceivedHandler? DeviceMessageReceivedAsync;
+
+    DeviceInfo? NetworkCoordinatorInfo { get; }
+
+    void StartZigBeeNetwork();
+}
+
+public sealed class NetworkHandler(INetworkCoordinatorService networkCoordinatorService,
+        INetworkDevice networkDevice,
+        ILogger<NetworkHandler> logger) : INetworkHandler
+{
+    public event DeviceAnnouncedHandler? DeviceAnnouncedAsync
+    {
+        add => _networkDevice.DeviceAnnouncedAsync += value;
+        remove => _networkDevice.DeviceAnnouncedAsync -= value;
+    }
+
+    public event EndDeviceMessageReceivedHandler? DeviceMessageReceivedAsync
+    {
+        add => _networkDevice.DeviceMessageReceivedAsync += value;
+        remove => _networkDevice.DeviceMessageReceivedAsync -= value;
+    }
+
+    private readonly INetworkCoordinatorService _networkCoordinatorService = networkCoordinatorService;
+    private readonly INetworkDevice _networkDevice = networkDevice;
+    private readonly ILogger<NetworkHandler> _logger = logger;
+
+    public void StartZigBeeNetwork()
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("Starting ZigBee network.");
+    }
+}
--- LLDev.TI.CC2531.RxTx/Models/DeviceInfo.cs
+++ LLDev.TI.CC2531.RxTx/Models/DeviceInfo.cs
@@ -1,2 +1,2 @@
 ﻿namespace LLDev.TI.CC2531.RxTx.Models;
-internal sealed record DeviceInfo(ulong IeeeAddr, ushort NwkAddr);
+public sealed record DeviceInfo(ulong IeeeAddr, ushort NwkAddr);
