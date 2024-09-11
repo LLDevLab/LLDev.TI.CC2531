a9a4d7b76b09293e79b8ab25c671bb0e8a9029e3
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2024-09-11 20:45:37 +0300

    Adding ServiceCollectionExtensions class

---
+++ LLDev.TI.CC2531.RxTx/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using LLDev.TI.CC2531.RxTx.Devices;
+using LLDev.TI.CC2531.RxTx.Handlers;
+using LLDev.TI.CC2531.RxTx.Packets;
+using LLDev.TI.CC2531.RxTx.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LLDev.TI.CC2531.RxTx.Extensions;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddZigBeeServices(this IServiceCollection services)
+    {
+        services.AddSingleton<ISerialPortHandler, SerialPortHandler>();
+        services.AddSingleton<ISerialPortDataHandler, SerialPortDataHandler>();
+        services.AddSingleton<IPacketHeaderFactory, PacketHeaderFactory>();
+        services.AddSingleton<IPacketFactory, PacketFactory>();
+        services.AddSingleton<IPacketHandler, PacketHandler>();
+        services.AddSingleton<ITransactionService, TransactionService>();
+        services.AddSingleton<ICmdTypeValidationService, CmdTypeValidationService>();
+        services.AddSingleton<IAwaitedPacketCacheService, AwaitedPacketCacheService>();
+        services.AddSingleton<IPacketReceiverTransmitterService, PacketReceiverTransmitterService>();
+        services.AddSingleton<INetworkCoordinator, NetworkCoordinator>();
+        services.AddSingleton<INetworkCoordinatorService, NetworkCoordinatorService>();
+        services.AddSingleton<INetworkDevice, NetworkDevice>();
+        services.AddSingleton<INetworkHandler, NetworkHandler>();
+
+        services.AddTransient<ICriticalSectionService, CriticalSectionService>();
+
+        return services;
+    }
+}
